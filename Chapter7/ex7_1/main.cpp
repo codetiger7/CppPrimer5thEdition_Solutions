a1f55ac14a280c3d80399339d66894a72edd9410
codetiger7 <brianopedal@Brians-MacBook-Pro.local>
2015-10-02 21:34:12 +0200

    update

---
+++ Chapter7/ex7_1/main.cpp
@@ -0,0 +1,21 @@
+// Exercise 7.1
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Sales_data
+{
+    string bookNo_;
+    unsigned unitsSold_ = 0;
+
+
+};
+
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
---
+++ Chapter7/readme.md
@@ -0,0 +1,5 @@
+## [Exercise 7.1](ex7_1/main.cpp)
+
+> Write a version of the transaction-processing program from § 1.6 (p. 24) using the *Sales_data* class you defined for the exercises § 2.6.1 (p. 72).
+
+
